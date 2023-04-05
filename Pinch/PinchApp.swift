f925b112bd304038f584df5e601a6b2e1640c48f
Timthom <monsson47thomas@gmail.com>
2023-04-05 12:37:30 +0200

    first commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Thomas Månsson on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
