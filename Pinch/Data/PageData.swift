3f9c0d2b3ff850a91efaf30f1e75314ddb124ddd
Timthom <monsson47thomas@gmail.com>
2023-04-05 18:15:29 +0200

    added thumbnails and model + Data

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Thomas Månsson on 05/04/2023.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
