93a808392df902d1c366be18659e8d74dbf9afa4
Timthom <monsson47thomas@gmail.com>
2023-04-05 16:54:06 +0200

    Adding Drag gesture and Long Press Gesture and Controll Interface

--- Pinch/ Screen/ContentView.swift
+++ Pinch/ Screen/ContentView.swift
@@ -13,14 +13,23 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -29,6 +38,8 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                // NOTE: .imageOffset must come before .scaleEffect
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -37,11 +48,25 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                           resetImageState()
+                            
                         }
                     })//: TAP GESTURE
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                                
+                            }
+                    )//: DRAG GESTURE
             }//: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -50,6 +75,68 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+            , alignment: .top
+            )
+        // MARK: - CONTROLS
+            .overlay(
+                Group {
+                    HStack {
+                        // SCALE DOWN
+                        
+                        Button {
+                            //Some action
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                           ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        //RESET
+                        Button {
+                            //Some action
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+
+                        //SCALE UP
+                        Button {
+                            //Some action
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+
+                    }//: CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            )
         }//: NAVIGATION
         .navigationViewStyle(.stack)
     }
---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Thomas Månsson on 05/04/2023.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
